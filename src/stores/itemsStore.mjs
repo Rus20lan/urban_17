a0e67f01bf4aeb7cde4105f9017ac52e3faf1b2e
Ruslan <ruslas@yandex.ru>
2024-11-20 23:38:55 +0300

    mode itemsStore, add timeout 3000  in fetch

--- src/stores/itemsStore.mjs
+++ src/stores/itemsStore.mjs
@@ -1,21 +1,23 @@
-import { createEffect, createEvent, createStore } from "effector";
+import { createEffect, createEvent, createStore } from 'effector';
 
 export const addItem = createEvent();
 export const updateFailedField = createEvent();
 // export const loadingProducts = createEvent();
 
 export const fetchProductFx = createEffect(async (id) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+    signal: AbortSignal.timeout(3000),
+  });
 
   if (!res.ok) {
-    throw new Error("Ошибка в получении данных");
+    throw new Error('Ошибка в получении данных');
   }
   return await res.json();
 });
 
 export const $itemsStore = createStore({
   products: [],
-  failed: "",
+  failed: '',
 })
   .on(addItem, (state, payload) =>
     Object.assign({}, { ...state }, { products: [...state.products, payload] })
@@ -25,7 +27,7 @@ export const $itemsStore = createStore({
     Object.assign(
       {},
       { ...state },
-      { products: [...state.products, payload], failed: "" }
+      { products: [...state.products, payload], failed: '' }
     )
   )
   .on(fetchProductFx.failData, (state, payload) =>
